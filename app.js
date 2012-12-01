f4c684dda840b1d30fa95e964d84b81a3d91c6e4
Patrick Ewing <patrick.henry.ewing@gmail.com>
2012-12-01 12:35:45 -0800

    Added index page with firebase-subscribed image tag

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var Firebase  = require('./lib/firebase-node');
 
 var node = new Firebase('https://drone.firebaseio.com/snaps');
 
-
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 app.listen(3000);
